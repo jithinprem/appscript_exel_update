b7b32bb061a6851f7dc9e929e2e48965edb04a94
Jithin <jithinprem43@gmail.com>
2023-04-19 03:16:57 +0530

    added the heading

--- appscript.js
+++ appscript.js
@@ -13,6 +13,16 @@ function doPost(e){
 function addUser(e){
   Logger.log(e.postData.contents);
   var user = JSON.parse(e.postData.contents);
+  var lastRow = sheet.getLastRow();
+  Logger.log('the value for no of rows = ');
+  Logger.log(lastRow);
+  if(lastRow == 0){
+    sheet.appendRow(['P_ID', 'P_NAME', 'DATE', 'PAIN', 'TIREDNESS', 'DROWSINESS', 'NAUSEA', 'LACK APPETITE', 'SHORTNESS BREATH', 'DEPRESSION', 'ANXIETY', 'WELL BEING', 'ADDITIONAL COMMENTS']);
+    Logger.log('successfully added');
+  }
+  else{
+    Logger.log('what happened');
+  }
   sheet.appendRow([user.id, user.name, user.date, user.pain, user.tiredness, user.drowsiness, user.nausea, user.lack_appetite, user.short_breath, user.depression, user.anxiety, user.well_being, user.additional_cmt ]);
 
   // step4
